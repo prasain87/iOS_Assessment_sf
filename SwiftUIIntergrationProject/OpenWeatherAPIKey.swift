82ab28960cace014f8331c01a81b4c358643499e
Prateek Sujaina <prateek.sujaina@gmail.com>
2024-05-31 20:50:34 +0530

    add Open weathe API Key

--- SwiftUIIntergrationProject/OpenWeatherAPIKey.swift
+++ SwiftUIIntergrationProject/OpenWeatherAPIKey.swift
@@ -8,4 +8,4 @@
 import Foundation
 
 //TODO: Enter your Open weather API Key from: https://home.openweathermap.org/api_keys
-let openWeatherAPIKey: String = ""
+let openWeatherAPIKey: String = "21272b1e4e2460d9bcea45117341805b"
