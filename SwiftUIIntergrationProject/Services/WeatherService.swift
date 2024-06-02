6dc73b0aa281ca4719b10b8eaacb9b7b8616e6eb
Prateek Sujaina <prateek.sujaina@gmail.com>
2024-06-02 21:24:38 +0530

    add API implementation to fetch current-weather and weather-forcast from remote

--- SwiftUIIntergrationProject/Services/WeatherService.swift
+++ SwiftUIIntergrationProject/Services/WeatherService.swift
@@ -1,5 +1,5 @@
 import Foundation
-import Combine
+import RxSwift
 import MapKit
 
   // TODO: Fill in this to retrieve current weather, and 5 day forecast by passing in a CLLocation
@@ -12,4 +12,47 @@ struct WeatherService {
 
 extension WeatherService {
   static var live = WeatherService()
+}
+
+extension WeatherService {
+    func retrieveWeatherForecast(_ location: CLLocation) -> Observable<ForecastJSONData?> {
+        guard let url = forecastURL(location: location) else {
+            return .just(nil)
+        }
+        return data(url: url)
+    }
+    
+    func retrieveCurrentWeather(_ location: CLLocation) -> Observable<CurrentWeatherJSONData?> {
+        guard let url = currentWeatherURL(location: location) else {
+            return .just(nil)
+        }
+        return data(url: url)
+    }
+    
+    func data<T: Decodable>(url: URL) -> Observable<T?> {
+        let request: URLRequest = URLRequest(url: url)
+        return Observable.create { obs in
+            let task = URLSession.shared.dataTask(with: request) { data, rsp, error in
+                if let error {
+                    obs.onError(SimpleError.dataLoad(error.localizedDescription))
+                } else if let data {
+                    do {
+                        let decoded = try JSONDecoder().decode(T.self, from: data)
+                        obs.onNext(decoded)
+                    } catch {
+                        obs.onError(SimpleError.dataParse(error.localizedDescription))
+                        print(error)
+                    }
+                } else {
+                    obs.onNext(nil)
+                }
+                obs.onCompleted()
+            }
+            task.resume()
+            
+            return Disposables.create {
+                task.cancel()
+            }
+        }
+    }
 }
---
+++ SwiftUIIntergrationProject/UIKitReactiveSwift/CurrentWeatherView.swift
@@ -0,0 +1,8 @@
+//
+//  CurrentWeatherView.swift
+//  SwiftUIIntergrationProject
+//
+//  Created by Prateek Sujaina on 02/06/24.
+//
+
+import Foundation
